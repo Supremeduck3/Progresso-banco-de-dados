a4819d577196a6b749ac5f7ed268c17adb3e0b92
Supremeduck008 <pedro.otaviobraga@gmail.com>
2025-09-22 09:16:42 -0300

    feat: contar quantidade

--- queries/04-delete.sql
+++ queries/04-delete.sql
@@ -1,2 +1,12 @@
 DELETE FROM usarios 
-WHERE username = 'CheatCodeKing;'
+WHERE username = 'CheatCodeKing';
+
+DELETE FROM usarios
+WHERE id = 13;
+
+DELETE FROM usarios
+WHERE pontos < 2000;
+
+SELECT * FROM usarios;
+
+SELECT COUNT (*) as Total_usuarios FROM usuarios;
